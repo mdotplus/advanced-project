6d25735df78aed7af063e81d9a64bc34f299c8b3
mdotplus <sun.eat.summer@gmail.com>
2024-09-18 14:52:42 +0900

    Authoritiesテーブルのseeder作成

---
+++ src/database/seeders/AuthoritiesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AuthoritiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'authority' => 1,
+        ];
+        DB::table('authors')->insert($param);
+        $param = [
+            'authority' => 2,
+        ];
+        DB::table('authors')->insert($param);
+        $param = [
+            'authority' => 3,
+        ];
+        DB::table('authors')->insert($param);
+    }
+}
