0b0ab34dc73073c550d725b71fee98873689d3ad
mdotplus <sun.eat.summer@gmail.com>
2024-09-21 01:33:05 +0900

    メール認証機能実装

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -1,21 +1,50 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/fortify-common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/verify-email.css') }}">
 @endsection
 
 @section('content')
-    <div class="verify-body verify-contents">
-        <p>ログイン認証メールを送信しました</p>
-        <p>60分以内に認証を行ってください</p>
+    <div class="card">
+        <div class="card__contents verify-body">
+            <div class="verify-contents">
+                <p>ログイン認証メールを送信しました</p>
+                <p>60分以内に認証を行ってください</p>
+            </div>
+            <div class="verify-again">
+                <p>メールが届いていませんか？</p>
+                <p>もう一度認証メールを送信する場合は<br>こちらをクリック</p>
+                <form action="/email/verification-notification" method="post">
+                    @csrf
+                    <button class="button-resend" type="submit">認証メールを再送信</button>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="verify-body verify-again">
-        <p>メールが届いていませんか？</p>
-        <p>もう一度認証メールを送信する場合はこちらをクリック</p>
-        <form action="/email/verification-notification" method="post">
+
+        <form class="card__contents" action="/register" method="post">
             @csrf
-            <button class="button-resend" type="submit">認証メールを再送信</button>
+            <div>
+                <img src="img/person.png" alt="人のアイコン">
+                <input class="card__contents__input-box" type="text" name="name" value="{{ old('name') }}" placeholder="Username">
+                @error('name')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <img src="img/email.png" alt="メールのアイコン">
+                <input class="card__contents__input-box" type="text" name="email" value="{{ old('email') }}" placeholder="Email">
+                @error('email')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <img src="img/password.png" alt="鍵のアイコン">
+                <input class="card__contents__input-box" type="password" name="password" placeholder="Password">
+                @error('password')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <button class="card__contents--button-submit" type="submit">登録</button>
         </form>
-    </div>
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['verified', 'auth']], function () {
     Route::get('/', [IndexController::class, 'index']);
 });
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
