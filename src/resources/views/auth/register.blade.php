2cdc89f4408206c53901d2cdbad0107d5f2f9f01
mdotplus <sun.eat.summer@gmail.com>
2024-09-23 01:42:51 +0900

    ファイルの読み込みにasset関数追記

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts/app')
+@extends ('layouts/app')
 
-@section('css')
+@section ('css')
     <link rel="stylesheet" href="{{ asset('css/fortify-common.css') }}">
 @endsection
 
-@section('content')
+@section ('content')
     <div class="card">
         <div class="card__title">
             Registration
@@ -12,23 +12,23 @@
         <form class="card__contents" action="/register" method="post">
             @csrf
             <div>
-                <img src="img/person.png" alt="人のアイコン">
+                <img src="{{ asset('img/person.png') }}" alt="人のアイコン">
                 <input class="card__contents__input-box" type="text" name="name" value="{{ old('name') }}" placeholder="Username">
-                @error('name')
+                @error ('name')
                     <div class="error">{{ $message }}</div>
                 @enderror
             </div>
             <div>
-                <img src="img/email.png" alt="メールのアイコン">
+                <img src="{{ asset('img/email.png') }}" alt="メールのアイコン">
                 <input class="card__contents__input-box" type="text" name="email" value="{{ old('email') }}" placeholder="Email">
-                @error('email')
+                @error ('email')
                     <div class="error">{{ $message }}</div>
                 @enderror
             </div>
             <div>
-                <img src="img/password.png" alt="鍵のアイコン">
+                <img src="{{ asset('img/password.png') }}" alt="鍵のアイコン">
                 <input class="card__contents__input-box" type="password" name="password" placeholder="Password">
-                @error('password')
+                @error ('password')
                     <div class="error">{{ $message }}</div>
                 @enderror
             </div>
