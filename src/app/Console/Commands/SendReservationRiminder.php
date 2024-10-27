3ecd2ebfaced6bea9f7c659dc4d2f2e962684149
mdotplus <sun.eat.summer@gmail.com>
2024-10-27 22:33:57 +0900

    リマインダーを送信するメールとコマンドを実装

---
+++ src/app/Console/Commands/SendReservationRiminder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Reminder;
+
+class SendReservationRiminder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:reminder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send reservation reminder email at 8 AM';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Mail::to('admin@rese.com')->send(new Reminder());
+        $this->info('Daily report email sent successfully.');
+    }
+}
